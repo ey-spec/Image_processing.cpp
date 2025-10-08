6b084ae50fc179e12d1951f794179b4e1b1b51b0
KarimAshraf4115 <karim.ashraf.4115@gmail.com>
2025-10-08 03:57:57 +0300

    a3333.cpp

--- filters.cpp
+++ filters.cpp
@@ -25,6 +25,9 @@ Team Members:
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
 #include "Image_Class.h"
 using namespace std;
 void Display_Menu() {
@@ -38,19 +41,19 @@ void Display_Menu() {
     cout << "7. Rotate Image\n";
     cout << "8. Darken and Lighten Image\n";
     cout << "9. Crop Images\n";
-    cout << "10. Adding a Frame to the Picture\n";
+    cout << "10. Adding a Frame to the Picture\n"; //
     cout << "11. Detect Image Edges\n";
     cout << "12. Resizing Images\n";
-    cout << "13. Blur Images\n";
-    cout << "14. Natural Sunlight.\n";
-    cout << "15. Oil painting\n";
-    cout << "16. TV Filter\n";
+    cout << "13. Blur Images\n"; //
+    cout << "14. Apply Natural Sunlight filter\n";
+    cout << "15. Apply Oil painting filter\n";
+    cout << "16. Apply TV noise Filter\n"; //
     cout << "17. Purple Image\n";
     cout << "18. Infrared Image\n";
-    cout << "19. Image skewing\n";
+    cout << "19. Image skewing\n"; //
     cout << "20. Save Current Image\n";
     cout << "21. Exit\n";
-    cout << "Choose option: ";
+    cout << "Choose option: "<<"\n";
 }
 
 // filter 1
@@ -142,6 +145,7 @@ Image resizeImage(const Image &src, int new_W, int new_H) {
 }
 
 // filter 4
+
 void Apply_Merge_filter(Image &img) {
     string filename2;
     cout << "Enter the filename of the second image: ";
@@ -255,7 +259,9 @@ void Apply_Flip_filter(Image &img) {
 }
 
 // filter 6
+
 void Apply_rotate_filter(Image &img, int angle){
+
     int width = img.width;
     int height = img.height;
     Image rotated_image;
@@ -300,7 +306,9 @@ void Apply_rotate_filter(Image &img, int angle){
     img = rotated_image;
     cout << "[Rotate filter is applied]\n";
 }
+
 // filter 7 (custom percentage)
+
 void Apply_Lighten_Darken_filter(Image &img) {
     cout << "Choose an option:\n";
     cout << "1 - Lighten Image (make brighter)\n";
@@ -345,7 +353,9 @@ void Apply_Lighten_Darken_filter(Image &img) {
     else if (choice == 2)
         cout << "[Darken filter applied by " << percentage << "%]\n";
 }
+
 //filter 8 
+
 void Apply_Crop_filter(Image& img) {
     int x, y, W, H;
     cout << "Enter starting point (x y): ";
@@ -370,7 +380,160 @@ void Apply_Crop_filter(Image& img) {
     cout << "[Crop filter applied successfully]\n";
 }
 
+// a function that take colors from user and apply it 
+
+void get_colors_from_user(int choice, unsigned char &r, unsigned char &g, unsigned char &b) {
+    if (choice == 1) { r=0; g=0; b=0; }             // Black
+    else if (choice == 2) { r=255; g=255; b=255; }  // White
+    else if (choice == 3) { r=0; g=255; b=0; }      // Green
+    else if (choice == 4) { r=0; g=0; b=255; }      // Blue
+    else if (choice == 5) { r=128; g=0; b=128; }    // Purple
+    else if (choice == 6) { r=255; g=165; b=0; }    // Orange
+    else if (choice == 7) { r=255; g=255; b=0; }    // Yellow
+    else if (choice == 8) { r=255; g=0; b=0; }      // Red
+    else if (choice == 9) { r=255; g=192; b=203; }  // Pink
+    else if (choice == 10) { r=165; g=42; b=42; }   // Brown
+    else if (choice == 11) { r=128; g=128; b=128; } // Gray
+    else if (choice == 12) { r=255; g=215; b=0; }   // Gold
+    else { r=0; g=0; b=0; }                         // Default Black
+}
+
+// Corner decorations
+
+void draw_Corner_Decorations(Image &img, int thickness, unsigned char r, unsigned char g, unsigned char b) {
+    int size = thickness * 2;
+
+    // Top-left
+    for (int y = 0; y < size; y++) {
+        for (int x = 0; x < size; x++) {
+            if (x < thickness || y < thickness) {
+                img.setPixel(x, y, 0, r);
+                img.setPixel(x, y, 1, g);
+                img.setPixel(x, y, 2, b);
+            }
+        }
+    }
+
+    // Top-right
+    for (int y = 0; y < size; y++) {
+        for (int x = img.width - size; x < img.width; x++) {
+            if (x > img.width - thickness || y < thickness) {
+                img.setPixel(x, y, 0, r);
+                img.setPixel(x, y, 1, g);
+                img.setPixel(x, y, 2, b);
+            }
+        }
+    }
+
+    // Bottom-left
+    for (int y = img.height - size; y < img.height; y++) {
+        for (int x = 0; x < size; x++) {
+            if (x < thickness || y > img.height - thickness) {
+                img.setPixel(x, y, 0, r);
+                img.setPixel(x, y, 1, g);
+                img.setPixel(x, y, 2, b);
+            }
+        }
+    }
+
+    // Bottom-right
+    for (int y = img.height - size; y < img.height; y++) {
+        for (int x = img.width - size; x < img.width; x++) {
+            if (x > img.width - thickness || y > img.height - thickness) {
+                img.setPixel(x, y, 0, r);
+                img.setPixel(x, y, 1, g);
+                img.setPixel(x, y, 2, b);
+            }
+        }
+    }
+}
+
+
+// Filter 9
+void Apply_frame_filter(Image &img) {
+    int thickness, colorChoice, style;
+    unsigned char r1,g1,b1,r2,g2,b2;
+
+    cout << "Enter frame thickness: ";
+    cin >> thickness;
+
+    cout << "Choose frame style:\n";
+    cout << "1. Solid color\n";
+    cout << "2. Gradient (between two colors)\n";
+    cout << "3. Striped (horizontal stripes)\n";
+    cout << "4. Checkered (chess pattern)\n";
+    cout << "5. Decorated Frame (with corners)\n";
+    cin >> style;
+
+    if (style == 1 || style == 3 || style == 4 || style == 5) {
+        cout << "Choose color:\n"
+             << "1. Black   2. White   3. Green\n"
+             << "4. Blue    5. Purple  6. Orange\n"
+             << "7. Yellow  8. Red     9. Pink\n"
+             << "10. Brown  11. Gray   12. Gold\n";
+
+        cin >> colorChoice;
+        get_colors_from_user(colorChoice, r1, g1, b1);
+    }
+    else if (style == 2) { // Gradient
+        cout << "Choose first color:\n";
+        cin >> colorChoice;
+        get_colors_from_user(colorChoice, r1, g1, b1);
+
+        cout << "Choose second color:\n";
+        cin >> colorChoice;
+        get_colors_from_user(colorChoice, r2, g2, b2);
+    }
+
+    if (thickness <= 0 || thickness > img.width/2 || thickness > img.height/2) {
+        cout << "Invalid thickness value!\n";
+        return;
+    }
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            bool isBorder = (y < thickness || y >= img.height - thickness ||
+                             x < thickness || x >= img.width - thickness);
+            if (!isBorder) continue;
+
+            unsigned char r=0,g=0,b=0;
+
+            if (style == 1) { // Solid
+                r=r1; g=g1; b=b1;
+            }
+            else if (style == 2) { // Gradient
+                float t = (float)(x + y) / (img.width + img.height);
+                r = (1 - t) * r1 + t * r2;
+                g = (1 - t) * g1 + t * g2;
+                b = (1 - t) * b1 + t * b2;
+            }
+            else if (style == 3) { // Striped
+                if ((y / 10) % 2 == 0) { r=r1; g=g1; b=b1; }
+                else { r=255; g=255; b=255; }
+            }
+            else if (style == 4) { // Checkered
+                if (((x/10) + (y/10)) % 2 == 0) { r=r1; g=g1; b=b1; }
+                else { r=255; g=255; b=255; }
+            }
+            else if (style == 5) { // Decorated (base frame first)
+                r=r1; g=g1; b=b1;
+            }
+
+            img.setPixel(x, y, 0, r);
+            img.setPixel(x, y, 1, g);
+            img.setPixel(x, y, 2, b);
+        }
+    }
+
+    if (style == 5) {
+        draw_Corner_Decorations(img, thickness, 255, 255, 255); // زخرفة بالأبيض
+    }
+}
+
+
+
 // filter 10
+
 void Apply_Edge_Detection_filter(Image &img) {
     Image edgeImage(img.width, img.height);
 
@@ -407,7 +570,9 @@ void Apply_Edge_Detection_filter(Image &img) {
     img = edgeImage;
     cout << "[Edge Detection filter applied successfully]\n";
 }
+
 // filter 11 
+
 void Apply_Resize_filter(Image& img) {
     int new_W, new_H;
     cout << "Enter new width and height: ";
@@ -432,7 +597,60 @@ void Apply_Resize_filter(Image& img) {
     img = resized;
     cout << "[Resize filter applied successfully]\n";
 }
+
+
+
+// filter 12
+
+void Apply_Blur_Filter(Image &img) {
+    int strength;
+    cout << "Enter blur strength (1 (light) , 5 (mid) , 10 (heavy) ): ";
+    cin >> strength;
+
+    if (strength <= 0) {
+        cout << "Invalid strength!\n";
+        return;
+    }
+
+    Image temp = img;
+
+    int offset = strength;
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            long sumR = 0, sumG = 0, sumB = 0;
+            int count = 0;
+
+            for (int ky = -offset; ky <= offset; ky++) {
+                for (int kx = -offset; kx <= offset; kx++) {
+                    int nx = x + kx;
+                    int ny = y + ky;
+
+                    if (nx >= 0 && nx < img.width && ny >= 0 && ny < img.height) {
+                        sumR += temp.getPixel(nx, ny, 0);
+                        sumG += temp.getPixel(nx, ny, 1);
+                        sumB += temp.getPixel(nx, ny, 2);
+                        count++;
+                    }
+                }
+            }
+
+            unsigned char r = sumR / count;
+            unsigned char g = sumG / count;
+            unsigned char b = sumB / count;
+
+            img.setPixel(x, y, 0, r);
+            img.setPixel(x, y, 1, g);
+            img.setPixel(x, y, 2, b);
+        }
+    }
+}
+
+
+
+
 // filter 13
+
 void Apply_Sunlight_filter(Image &img) {
     int width = img.width;
     int height = img.height;
@@ -459,7 +677,50 @@ void Apply_Sunlight_filter(Image &img) {
 
     cout << "[Sunlight filter applied]\n";
 }
+
+
+
+
+// Filter 15
+void Apply_TV_Noise_Filter(Image &img) {
+    srand(time(0)); 
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+
+            unsigned char r = img.getPixel(x, y, 0);
+            unsigned char g = img.getPixel(x, y, 1);
+            unsigned char b = img.getPixel(x, y, 2);
+
+      
+            unsigned char gray = (r + g + b) / 3;
+            r = (r + gray) / 2;
+            g = (g + gray) / 2;
+            b = (b + gray) / 2;
+
+        
+            int noise = (rand() % 100) - 50; 
+            int nr = max(0, min(255, r + noise));
+            int ng = max(0, min(255, g + noise));
+            int nb = max(0, min(255, b + noise));
+
+            if (y % 3 == 0) {
+                nr = nr / 2;
+                ng = ng / 2;
+                nb = nb / 2;
+            }
+
+            img.setPixel(x, y, 0, nr);
+            img.setPixel(x, y, 1, ng);
+            img.setPixel(x, y, 2, nb);
+        }
+    }
+}
+
+
+
 // filter 16
+
 void Apply_Purple_filter(Image &img) {
 
     for (int y = 0; y < img.height; y++) {
@@ -482,6 +743,69 @@ void Apply_Purple_filter(Image &img) {
     }
 
     cout << "[Purple filter applied]\n";
+}
+
+
+// Filter 18
+void Apply_Skew_Filter(Image &img) {
+    int degree;
+    char direction;
+
+    cout << "Enter skew degree: ";
+    cin >> degree;
+
+    cout << "Choose direction (R = right, L = left): ";
+    cin >> direction;
+
+    if (degree == 0) {
+        cout << "No skew applied.\n";
+        return;
+    }
+
+    Image temp = img;
+
+    double angle = degree * M_PI / 180.0;
+    double shear = tan(angle);
+
+    // حساب أقصى إزاحة
+    int maxShift = (int)(fabs(shear) * img.height);
+    int newWidth = img.width + maxShift;
+
+    Image output(newWidth, img.height);
+
+    // خلفية بيضاء
+    for (int y = 0; y < output.height; y++) {
+        for (int x = 0; x < output.width; x++) {
+            output.setPixel(x, y, 0, 255);
+            output.setPixel(x, y, 1, 255);
+            output.setPixel(x, y, 2, 255);
+        }
+    }
+
+    // skew يمين أو شمال
+    for (int y = 0; y < img.height; y++) {
+        int shift = (int)(shear * y);
+
+        for (int x = 0; x < img.width; x++) {
+            int newX;
+
+            if (direction == 'R' || direction == 'r') {
+                // skew يمين
+                newX = x + shift;
+            } else {
+                // skew شمال
+                newX = x - shift + maxShift;
+            }
+
+            if (newX >= 0 && newX < output.width) {
+                output.setPixel(newX, y, 0, temp.getPixel(x, y, 0));
+                output.setPixel(newX, y, 1, temp.getPixel(x, y, 1));
+                output.setPixel(newX, y, 2, temp.getPixel(x, y, 2));
+            }
+        }
+    }
+
+    img = output;
 }
 
 
@@ -555,7 +879,7 @@ int main() {
         }
         else if (choice == 7) {
             int angle;
-            cout<< "Enter angle from (90,180,270): " ;
+            cout<< "Enter angle from (90, 180, 270): " ;
             cin>>angle;
             Apply_rotate_filter(img,angle);
             modified = true;
@@ -568,16 +892,34 @@ int main() {
             Apply_Crop_filter(img);
             modified = true;
         }
+        else if (choice == 10) {
+            Apply_frame_filter(img);
+            modified = true;
+        }
         else if (choice == 11) {
             Apply_Edge_Detection_filter(img);
+            modified = true;
+        }
+        else if (choice == 13) {
+            Apply_Blur_Filter(img);
             modified = true;
         }
         else if (choice == 14) {
             Apply_Sunlight_filter(img);
             modified = true;
         }
+        
+        else if (choice == 16) {
+            Apply_TV_Noise_Filter(img);
+            modified = true;
+        }
         else if (choice == 17) {
             Apply_Purple_filter(img);
+            modified = true;
+        }
+        
+        else if (choice == 19) {
+            Apply_Skew_Filter(img);
             modified = true;
         }
         else if (choice == 20) {
@@ -617,7 +959,6 @@ int main() {
     }
     return 0;
 }
-
 
 
 
